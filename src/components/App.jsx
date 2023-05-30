5f74a6b9502211c2278b92536ade2b8149322553
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-30 18:35:18 +0300

    try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Searchbar from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  setQuery = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div
+      // style={{
+      //   // height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
+      >
+        <Searchbar setQuery={this.setQuery} />
+        <ImageGallery query={this.state.query} />
+      </div>
+    );
+  }
+}
